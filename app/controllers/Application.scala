e58047e78f1eeed03208306287ea8f61b87d5272
bytehala <bytehala@users.noreply.github.com>
2016-09-18 13:02:26 +0800

    Develop (#3)
    * SunInfo. Author's samples are not working, so I used the DI which comes free with Play FW anyway.
    
    * Fix broken build

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,12 +3,15 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.libs.ws._
+import play.api.Play.current
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
-class Application extends Controller {
+class Application @Inject() (ws: WSClient) extends Controller {
 
   /**
    * Create an Action to render an HTML page with a welcome message.
@@ -16,14 +19,29 @@ class Application extends Controller {
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index = Action {
+  def index = Action.async {
     import java.util.Date
     import java.text.SimpleDateFormat
+    import scala.concurrent.Future
 
     val date = new Date()
     val dateStr = new SimpleDateFormat().format(date)
 
-    Ok(views.html.index(dateStr))
+    // Future.successful {
+    //   Ok(views.html.index(dateStr))
+    // }
+
+    import models.SunInfo
+
+    val responseF = ws.url("http://api.sunrise-sunset.org/json?" +
+        "lat=-33.8830&lng=151.2167&formatted=0").get()
+    responseF.map { response =>
+        val json = response.json
+        val sunriseTimeStr = (json \ "results" \ "sunrise").as[String]
+        val sunsetTimeStr = (json \ "results" \ "sunset").as[String]
+        val sunInfo = SunInfo(sunriseTimeStr, sunsetTimeStr)
+        Ok(views.html.index(sunInfo))
+    }
   }
 
 }
